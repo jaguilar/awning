de40c0d1db7d7518d8bc5bfe53eb24619ccad435
James Aguilar <799564+jaguilar@users.noreply.github.com>
2024-05-05 23:01:38 -0400

    Add code for reading the button states.

--- src/awning.cc
+++ src/awning.cc
@@ -1,90 +1,96 @@
-#include "SomfyRemote.h"
-
-#include "boards/pico_w.h"
-#include "hardware/flash.h"
-#include "hardware/regs/addressmap.h"
-#include "hardware/sync.h"
-#include "pico/multicore.h"
-#include "pico/platform.h"
-#include "portmacro.h"
 #include <cstdint>
-#include <span>
-#include <string_view>
+#include <cstring>
+#include <initializer_list>
 
-extern uint32_t __flash_binary_end;
+#include "FreeRTOS.h"
+#include "hardware/gpio.h"
+#include "hardware/irq.h"
+#include "pico/printf.h"
+#include "portmacro.h"
+#include "projdefs.h"
+#include "task.h"
 
 namespace jagsawning {
 
-class PicoFlashRCS : public RollingCodeStorage {
-public:
-  // We'll use up to 10 pages of flash for the rolling code storage, starting
-  // from the last page and working our way down. We won't use a page if it
-  // includes __flash_binary_end. We don't anticipate this binary growing to
-  // take up more than a small fraction of the total flash (it's 720kB as of
-  // this writing and the Pico has 2MB of flash).
-  PicoFlashRCS() {
-    // Let's just assert we have enough space for now.
-    assert(PICO_FLASH_SIZE_BYTES - __flash_binary_end > FLASH_PAGE_SIZE * 10);
+constexpr int kPinButtonUp = 20;
+constexpr int kPinButtonMy = 19;
+constexpr int kPinButtonDn = 18;
 
-    constexpr std::string_view kFlashDataMagic = "JAGSAWNING_STORAGE_0000";
-    
-    // The first flash page will contain a magic string to identify the protocol
-    // we're using, then one byte of 1s for each page we've used, up to 10.
+constexpr uint8_t kButtonUpShift = 0;
+constexpr uint8_t kButtonMyShift = 1;
+constexpr uint8_t kButtonDnShift = 2;
+constexpr uint8_t kButtonProgMask =
+    (1 << kButtonUpShift) | (1 << kButtonDnShift);
 
+uint8_t GetPressedButtons() {
+  return (static_cast<uint8_t>(!gpio_get(kPinButtonUp)) << kButtonUpShift) |
+         (static_cast<uint8_t>(!gpio_get(kPinButtonMy)) << kButtonMyShift) |
+         (static_cast<uint8_t>(!gpio_get(kPinButtonDn)) << kButtonDnShift);
+}
 
+bool IsUpPressed(uint8_t buttons) { return buttons & (1 << kButtonUpShift); }
+bool IsMyPressed(uint8_t buttons) { return buttons & (1 << kButtonMyShift); }
+bool IsDnPressed(uint8_t buttons) { return buttons & (1 << kButtonDnShift); }
 
+void button_task(void *) {
+  for (int pin : {kPinButtonUp, kPinButtonMy, kPinButtonDn}) {
+    gpio_init(pin);
+    gpio_set_dir(pin, GPIO_IN);
+    gpio_pull_up(pin);
   }
 
-  // Reads the flash at memory address. (This is just a memory read, thanks to
-  // XIP.)
-  static std::span<uint8_t> ReadFlash(std::intptr_t address, std::size_t size) {
-    return std::span<uint8_t>(reinterpret_cast<uint8_t *>(address), size);
-  }
+  static TaskHandle_t task_handle = xTaskGetCurrentTaskHandle();
+  gpio_set_irq_callback(+[](uint gpio, uint32_t event_mask) {
+    BaseType_t higher_priority_task_woken = pdFALSE;
+    vTaskNotifyGiveFromISR(task_handle, &higher_priority_task_woken);
+    portYIELD_FROM_ISR(higher_priority_task_woken);
+  });
+  gpio_set_irq_enabled(kPinButtonDn, GPIO_IRQ_EDGE_FALL, true);
+  gpio_set_irq_enabled(kPinButtonUp, GPIO_IRQ_EDGE_FALL, true);
+  gpio_set_irq_enabled(kPinButtonMy, GPIO_IRQ_EDGE_FALL, true);
+  while (true) {
+    xTaskNotifyWait(0b1, 0b1, nullptr, portMAX_DELAY);
 
-  static void WriteFlash(std::intptr_t address, std::span<uint8_t> data) {
-    static std::intptr_t flash_offset = address - XIP_BASE;
-    assert(flash_offset % FLASH_PAGE_SIZE == 0);
-    assert(data.size() % FLASH_PAGE_SIZE == 0);
-    assert(flash_offset + data.size() < PICO_FLASH_SIZE_BYTES);
-#ifndef NDEBUG
-    // Check that all bits which are 0 in old are also 0 in data. If not,
-    // we're trying to set a bit to 1. That is not possible with flash.
-    std::string_view old = ReadFlash(address, data.size());
-    for (int i = 0; i < old.size(); ++i) {
-      const unsigned char old_zero_bits = ~old[i];
-      const unsigned char new_one_bits = data[i];
-      if (old_zero_bits & new_one_bits) {
-        panic(
-            "at memory offset %d, trying to set a bit to 1 which is 0 in flash "
-            "(old: %02x, new: %02x)",
-            address + i, old[i], data[i]);
+    constexpr int kMsToCheck = 150;
+    constexpr int kDelayMs = 25;
+    constexpr int kTimesToCheck = kMsToCheck / kDelayMs;
+
+    uint8_t pressed = GetPressedButtons();
+    for (int i = 0; i < kTimesToCheck; ++i) {
+      // Wait for the pressed buttons to be stable for
+      vTaskDelay(pdMS_TO_TICKS(kDelayMs));
+      uint8_t newpressed = GetPressedButtons();
+      if (newpressed == 0) {
+        pressed = 0;
+        break;
+      } else if (newpressed != pressed) {
+        pressed = newpressed;
       }
     }
-#endif
-    vTaskEnterCritical();
-    multicore_lockout_start_blocking();
-    flash_range_program(flash_offset, data.data(), data.size());
-    multicore_lockout_end_blocking();
-    vTaskExitCritical();
+
+    if (IsUpPressed(pressed) && IsDnPressed(pressed)) {
+      printf("prog\n");
+    } else if (IsUpPressed(pressed)) {
+      printf("up\n");
+    } else if (IsMyPressed(pressed)) {
+      printf("my\n");
+    } else if (IsDnPressed(pressed)) {
+      printf("dn\n");
+    }
+
+    while (GetPressedButtons() != 0) {
+      vTaskDelay(pdMS_TO_TICKS(kDelayMs));
+    }
+    vTaskDelay(pdMS_TO_TICKS(100));
   }
+}
 
-  static void EraseFlash(std::intptr_5 address, )
+void main_task() {
+  xTaskCreate(button_task, "button_task", 512, nullptr, 1, nullptr);
 
-      /**
-       * Get the next rolling code from the store. This should also increase the
-       * rolling code and store it persistently.
-       *
-       * @return next rolling code
-       */
-      uint16_t nextCode() override {
-    return 0;
-  }
+  vTaskDelay(portMAX_DELAY);
+}
 
-private:
-};
-
-void main_task() { PicoFlashRCS rcs; }
-
-} // namespace jagsawning
+}  // namespace jagsawning
 
 extern "C" void main_task(void *) { jagsawning::main_task(); }
