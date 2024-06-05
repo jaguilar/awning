4af34eca3cf829555f7b1821f2455c686a132a49
James Aguilar <799564+jaguilar@users.noreply.github.com>
2024-06-04 20:59:07 -0400

    awning.cc in a testable state, but with dummy code and address

--- src/awning.cc
+++ src/awning.cc
@@ -1,6 +1,7 @@
 #include <cstdint>
 #include <cstdio>
 #include <cstring>
+#include <expected>
 #include <functional>
 #include <initializer_list>
 #include <memory>
@@ -10,9 +11,13 @@
 #include "FreeRTOS.h"
 #include "SomfyRemote.h"
 #include "event_groups.h"
+#include "freertosxx/event.h"
+#include "freertosxx/mutex.h"
+#include "freertosxx/queue.h"
 #include "hardware/gpio.h"
 #include "hardware/irq.h"
 #include "hardware/regs/intctrl.h"
+#include "homeassistant_service.h"
 #include "lwip/api.h"
 #include "lwip/apps/mqtt.h"
 #include "lwip/err.h"
@@ -34,6 +39,56 @@
 
 namespace jagsawning {
 namespace {
+// Combines the radio and the somfy remote class into a single controller
+// object.
+class SomfyController {
+ public:
+  static constexpr int kDataPin = 15;
+
+  static std::unique_ptr<SomfyController> Create() {
+    RfmSpiDriver driver = RfmSpiDriver::Create();
+    driver.SetMode(RfmSpiDriver::kSleep);
+    driver.SetCarrierFrequency(433'420'000);
+    driver.SetModulationType(RfmSpiDriver::kOok);
+    driver.SetDataMode(RfmSpiDriver::kContinuousWithoutSynchronizer);
+    driver.SetPower(0b1111'1111);
+
+    sleep_ms(100);  // Give the radio time to come up.
+
+    gpio_init(kDataPin);
+    gpio_set_dir(kDataPin, true);
+
+    return std::unique_ptr<SomfyController>(
+        new SomfyController(std::move(driver)));
+  }
+
+  // Yeah, Command and SomfyRemote are not namespaced. :(
+  void SendCommand(::Command c) {
+    printf("Waiting for transmit ready\n");
+    rfm_driver_.SetMode(RfmSpiDriver::kTransmit);
+    while (!(rfm_driver_.GetRegIrqFlags() & RfmSpiDriver::kTxReady)) {
+      sleep_ms(100);
+    }
+    printf("Transmit ready\n");
+    remote_.sendCommand(c, 0);
+    printf("Transmit done, sleeping\n");
+    rfm_driver_.SetMode(RfmSpiDriver::kSleep);
+  }
+
+ private:
+  SomfyController(RfmSpiDriver rfm_driver) : rfm_driver_(rfm_driver) {
+    remote_.setup();
+  }
+
+  RfmSpiDriver rfm_driver_;
+  // Remote address and RCS are just for testing. Will change to real storage
+  // for final release.
+  FixedRollingCodeStorage storage_{0x1234};
+  SomfyRemote remote_{kDataPin, 0x7357, &storage_};
+};
+std::unique_ptr<SomfyController> g_somfy_controller = nullptr;
+freertosxx::Mutex g_controller_mutex;
+
 constexpr int kPinButtonUp = 20;
 constexpr int kPinButtonMy = 19;
 constexpr int kPinButtonDn = 18;
@@ -57,39 +112,14 @@ bool IsDnPressed(uint8_t buttons) { return buttons & (1 << kButtonDnShift); }
 
 struct CommandMessage {
   // What was the somfy command?
-  Command command;
-
-  // Was the command generated manually via a button press?
-  bool manual;
+  ::Command command;
 };
 
 // Returns the CommandQueue used for passing from the command-generator tasks
 // to the radio task.
-static QueueHandle_t CommandQueue() {
-  static QueueHandle_t queue = xQueueCreate(1, sizeof(CommandMessage));
+static freertosxx::Queue<CommandMessage>& CommandQueue() {
+  static freertosxx::StaticQueue<CommandMessage, 1> queue;
   return queue;
-}
-
-// We don't worry about dropping commands because if it's user-generated then
-// the user can just press the button again and if it's automatically-generated
-// it's in conflict with the user command anyway.
-void TryPushCommand(const CommandMessage &m) {
-  xQueueSend(CommandQueue(), &m, 0);
-}
-
-// Returns the next command pushed to the queue. Blocks forever until a command
-// is pushed.
-void GetCommandBlocking(CommandMessage &m) {
-  while (xQueueReceive(CommandQueue(), &m, portMAX_DELAY) != pdTRUE) {
-  }
-}
-
-// Drains the command queue of any pending command. We throw away any commands
-// we received while we were transmitting a radio message before blocking.
-void DrainCommandQueue() {
-  CommandMessage m;
-  while (xQueueReceive(CommandQueue(), &m, 0) == pdTRUE) {
-  }
 }
 
 void button_task(void *) {
@@ -135,21 +165,29 @@ void button_task(void *) {
       }
     }
 
-    CommandMessage message{.manual = true};
+    ::Command command;
     if (IsUpPressed(pressed) && IsDnPressed(pressed)) {
-      message.command = Command::Prog;
+      command = Command::Prog;
     } else if (IsUpPressed(pressed)) {
-      message.command = Command::Up;
+      command = Command::Up;
     } else if (IsMyPressed(pressed)) {
-      message.command = Command::My;
+      command = Command::My;
     } else if (IsDnPressed(pressed)) {
-      message.command = Command::Down;
+      command = Command::Down;
     } else {
       printf("unexpected button combination, skipping\n");
       continue;
     }
     printf("Buttons: %02hhx\n", pressed);
-    TryPushCommand(message);
+    {
+      // Note that we explicitly do not update the state of the awning
+      // on homeassistant here, since this is morally equivalent to a
+      // button being pressed on a different remote, which we can't observe.
+      // I mean we could I guess but I would have to decode somfy commands
+      // and that seems like a lot of work.
+      freertosxx::MutexLock lock(g_controller_mutex);
+      g_somfy_controller->SendCommand(command);
+    }
 
     while (GetPressedButtons() != 0) {
       vTaskDelay(pdMS_TO_TICKS(kDelayMs));
@@ -158,318 +196,97 @@ void button_task(void *) {
   }
 }
 
-class RTOSQueueBase {
- public:
-  RTOSQueueBase(QueueHandle_t queue) : queue_(queue) {}
-  ~RTOSQueueBase() { vQueueDelete(queue_); }
-  RTOSQueueBase(const RTOSQueueBase &) = delete;
-  RTOSQueueBase(RTOSQueueBase &&) = delete;
-  RTOSQueueBase &operator=(const RTOSQueueBase &) = delete;
-  RTOSQueueBase &operator=(RTOSQueueBase &&) = delete;
+freertosxx::StaticEventGroup command_event;
 
-  void Drain() {}
-
- protected:
-  void SendUntyped(const void *message, TickType_t delay = portMAX_DELAY) {
-    xQueueSend(queue_, message, delay);
-  }
-  void ReceiveUntyped(void *message, TickType_t delay = portMAX_DELAY) {
-    xQueueReceive(queue_, message, delay);
-  }
-
- private:
-  QueueHandle_t queue_;
-};
-
-template <typename T>
-class RTOSTypedQueue : public RTOSQueueBase {
- public:
-  static_assert(
-      std::is_trivial_v<T> && std::is_standard_layout_v<T>,
-      "T must be a POD type");
-  RTOSTypedQueue(QueueHandle_t queue) : RTOSQueueBase(queue) {}
-
-  void Send(const T &message) { RTOSQueueBase::SendUntyped(&message); }
-  T Receive() {
-    T message;
-    RTOSQueueBase::ReceiveUntyped(&message);
-    return message;
-  }
-};
-
-template <typename T>
-class RTOSDynamicQueue : public RTOSTypedQueue<T> {
- public:
-  RTOSDynamicQueue(int size)
-      : RTOSTypedQueue<T>(xQueueCreate(size, sizeof(T))) {}
-};
-
-template <typename T, int Size>
-class RTOSStaticQueue : public RTOSTypedQueue<T> {
- public:
-  RTOSStaticQueue()
-      : RTOSTypedQueue<T>(xQueueCreateStatic(Size, sizeof(T), buf_, &queue_)) {}
-
- private:
-  StaticQueue_t queue_;
-  uint8_t buf_[Size * sizeof(T)];
-};
-
-class MqttClientWrapper {
-  using ErrCb = void (*)(void *, err_t);
-  ErrCb SendErrToQueue() {
-    return +[](void *arg, err_t err) {
-      reinterpret_cast<RTOSTypedQueue<err_t> *>(arg)->Send(err);
-    };
-  }
-
- public:
-  MqttClientWrapper() = delete;
-
-  using TopicFn =
-      std::move_only_function<void(std::string_view topic, int num_messages)>;
-  using MessageFn = std::move_only_function<void(
-      std::span<const uint8_t> message, uint8_t flags)>;
-
-  // Returns an MqttClientWrapper when the client has successfully connected,
-  // or else returns an err_t.
-  static std::expected<std::unique_ptr<MqttClientWrapper>, err_t> Create(
-      std::string_view host, uint16_t port,
-      const mqtt_connect_client_info_t &client_info, TopicFn topic_fn,
-      MessageFn message_fn) {
-    std::unique_ptr<MqttClientWrapper> wrapper(new MqttClientWrapper(
-        mqtt_client_new(), std::move(topic_fn), std::move(message_fn)));
-    err_t err = wrapper->Connect(host, port, client_info);
-    if (err != ERR_OK) {
-      return std::unexpected(err);
-    }
-    return wrapper;
-  }
-
-  err_t Publish(
-      std::string_view topic, std::string_view message, uint8_t qos,
-      uint8_t retain) {
-    RTOSStaticQueue<err_t, 1> err_queue;
-    err_t err = mqtt_publish(
-        client_.get(),
-        topic.data(),
-        message.data(),
-        message.size(),
-        qos,
-        retain,
-        SendErrToQueue(),
-        &err_queue);
-    err_t cb_err = err_queue.Receive();
-    if (err == ERR_OK) err = cb_err;
-    if (err != ERR_OK) {
-      printf(
-          "Error publishing message starting with %s (%d bytes): %s\n",
-          std::string(topic.substr(0, 10)).c_str(),
-          topic.size(),
-          lwip_strerr(err));
-    }
-    return err;
-  }
-
-  err_t Subscribe(std::string_view topic, uint8_t qos) {
-    return SubUnsub(topic, qos, true);
-  }
-  err_t Unsubscribe(std::string_view topic, uint8_t qos) {
-    return SubUnsub(topic, qos, false);
-  }
-
-  struct ChangeTopic {
-    std::string new_topic;
-    int num_messages;
-  };
-
- private:
-  MqttClientWrapper(
-      mqtt_client_t *client, TopicFn topic_fn, MessageFn message_fn)
-      : client_(client, mqtt_client_free),
-        topic_fn_(std::move(topic_fn)),
-        message_fn_(std::move(message_fn)) {}
-
-  err_t Connect(
-      std::string_view host, uint16_t port,
-      const mqtt_connect_client_info_t &client_info) {
-    ip_addr_t address;
-    err_t err = netconn_gethostbyname(MQTT_HOST, &address);
-    if (err != ERR_OK) {
-      printf("Error resolving host: %s, %s\n", MQTT_HOST, lwip_strerr(err));
-      return err;
-    }
-
-    RTOSStaticQueue<mqtt_connection_status_t, 1> connection_status_queue;
-    err = mqtt_client_connect(
-        client_.get(),
-        &address,
-        port,
-        +[](mqtt_client_s *, void *arg, mqtt_connection_status_t status) {
-          reinterpret_cast<decltype(connection_status_queue) *>(arg)->Send(
-              status);
-        },
-        &connection_status_queue,
-        &client_info);
-    if (err != ERR_OK) {
-      printf("Error connecting to MQTT: %s\n", lwip_strerr(err));
-      return err;
-    }
-    mqtt_connection_status_t connection_status =
-        connection_status_queue.Receive();
-    if (connection_status != MQTT_CONNECT_ACCEPTED) {
-      printf("Connection status != accepted: %d\n", connection_status);
-      return ERR_CONN;
-    }
-    printf("mqtt connected\n");
-    mqtt_set_inpub_callback(
-        client_.get(),
-        +[](void *arg, const char *buf, uint32_t remaining) {
-          reinterpret_cast<decltype(this)>(arg)->topic_fn_(
-              std::string_view(buf), remaining);
-        },
-        +[](void *arg, const uint8_t *buf, uint16_t len, uint8_t flags) {
-          reinterpret_cast<decltype(this)>(arg)->message_fn_(
-              std::span<const uint8_t>(buf, len), flags);
-        },
-        this);
-    return ERR_OK;
-  }
-
-  err_t SubUnsub(std::string_view topic, uint8_t qos, bool sub) {
-    RTOSStaticQueue<err_t, 1> err_queue;
-    err_t err = mqtt_sub_unsub(
-        client_.get(), topic.data(), qos, SendErrToQueue(), &err_queue, sub);
-    err_t cb_err = err_queue.Receive();
-    if (err == ERR_OK) err = cb_err;
-    if (err != ERR_OK) {
-      printf("Error subscribing to %s: %s\n", topic.data(), lwip_strerr(err));
-    }
-    printf("%ssubscribed: %s\n", sub ? "" : "un", topic.data());
-    return err;
-  }
-
-  std::unique_ptr<mqtt_client_t, decltype(&mqtt_client_free)> client_;
-  TopicFn topic_fn_;
-  MessageFn message_fn_;
-};
-
-void mqtt_task(void *) {
-  std::expected<std::unique_ptr<MqttClientWrapper>, err_t> mqtt =
-      std::unexpected(ERR_CONN);
-  while (!mqtt) {
-    mqtt = MqttClientWrapper::Create(
-        MQTT_HOST,
-        MQTT_PORT,
-        mqtt_connect_client_info_t{
-            .client_id = MQTT_CLIENT_ID,
-            .client_user = MQTT_USER,
-            .client_pass = MQTT_PASSWORD,
-        },
-        [&](std::string_view topic, int num_messages) {
-          // Do nothing. We only subscribe to one topic in this program,
-          // so we don't need to track what we're listening to.
-        },
-        [&](std::span<const uint8_t> message, uint8_t flags) {
-          printf("message received\n");
-          if (message.empty()) return;
-          CommandMessage m;
-          if (message[0] == 'c') {
-            m = {.command = Command::Up, .manual = false};
-          } else if (message[0] == 'C') {
-            // Critical wind speed. Shut immediately.
-            m = {.command = Command::Up, .manual = true};
-          } else if (message[0] == 'm') {
-            m = {.command = Command::My, .manual = false};
-          } else {
-            return;
-          }
-          TryPushCommand(m);
-        });
-    if (!mqtt) vTaskDelay(pdMS_TO_TICKS(5'000));
-  }
-
-  mqtt.value()->Subscribe(MQTT_TOPIC, 1);
-
-  while (true) {
-    vTaskDelay(portMAX_DELAY);
-  }
-}
-
-#ifndef SOMFY_RADIO_ADDRESS
-#error SOMFY_RADIO_ADDRESS must be defined.
-#endif
-
-void radio_task(void *) {
-  RfmSpiDriver driver = RfmSpiDriver::Create();
-  driver.SetMode(RfmSpiDriver::kSleep);
-  driver.SetCarrierFrequency(433'420'000);
-  driver.SetModulationType(RfmSpiDriver::kOok);
-  driver.SetDataMode(RfmSpiDriver::kContinuousWithoutSynchronizer);
-  driver.SetPower(0b1111'1111);
-
-  // Set the emitter pin up.
-  gpio_init(15);
-  gpio_set_dir(15, true);
-
-  PicoFlashRCS storage;
-  SomfyRemote remote(kPinRadioDio2, SOMFY_RADIO_ADDRESS, &storage);
-
-  // Radio is ready. Wait for a command.
-  // Note that we're recording the current time as the last message time because
-  absolute_time_t last_message_time = nil_time;
-  CommandMessage last_message{Command::My, false};
-  while (true) {
-    CommandMessage message;
-    printf("Waiting for command\n");
-    GetCommandBlocking(message);
+void homeassistant_task(void*) {
+  std::expected<std::unique_ptr<MqttClientWrapper>, err_t> client =
+      MqttClientWrapper::Create(MQTT_HOST, MQTT_PORT, ConnectInfoFromDefines());
+  if (!client) {
     printf(
-        "Received command %hhu manual=%d\n",
-        static_cast<byte>(message.command),
-        message.manual);
+        "failed to connsect to %s due to error %d\n",
+        MQTT_HOST,
+        client.error());
+  }
 
-    absolute_time_t receive_time = get_absolute_time();
+  freertosxx::EventGroup eg;
 
-    // Discard any automatic messages if a manual message has been received in
-    // the last hour.
-    if (last_message.manual && !message.manual &&
-        (is_nil_time(last_message_time) ||
-         absolute_time_diff_us(last_message_time, receive_time) <
-             (3600ll * 1'000'000))) {
-      printf("Automatic command received too recently after manual command\n");
-      continue;
+  std::expected<std::unique_ptr<HomeAssistantService>, err_t> service =
+      HomeAssistantService::Connect(
+          **client, [&](HomeAssistantService::Command cmd) {
+            printf("Received command %d\n", static_cast<int>(cmd));
+            command_event.Clear(
+                HomeAssistantService::kCmdClose |
+                HomeAssistantService::kCmdOpen |
+                HomeAssistantService::kCmdStop);
+            command_event.Set(cmd);
+          });
+  if (!service) {
+    panic(
+        "failed to connect to home assistant: %d\n",
+        lwip_strerr(service.error()));
+  }
+
+  HomeAssistantService::State current_state = HomeAssistantService::kClosed;
+  HomeAssistantService::State dest_state = HomeAssistantService::kClosed;
+  absolute_time_t time_when_state_change_done = at_the_end_of_time;
+
+  while (true) {
+    std::optional<TickType_t> timeout;
+    if (!is_at_the_end_of_time(time_when_state_change_done)) {
+      const int64_t ms_til_timeout =
+          absolute_time_diff_us(
+              get_absolute_time(), time_when_state_change_done) /
+          1000;
+      timeout = pdMS_TO_TICKS(ms_til_timeout);
     }
 
-    // We will send this command. Turn on the radio and wait for it to become
-    // ready.
-    driver.SetPower(0b1111'1111);
-    driver.SetMode(RfmSpiDriver::kTransmit);
-    printf("Waiting for tx ready.\n");
-    while (!(driver.GetRegIrqFlags() & RfmSpiDriver::kTxReady)) {
-      vTaskDelay(pdMS_TO_TICKS(10));
+    // Wait for a command to be received.
+    EventBits_t event = command_event.Wait(
+        HomeAssistantService::kCmdClose | HomeAssistantService::kCmdOpen |
+            HomeAssistantService::kCmdStop,
+        {.clear = true, .timeout = timeout});
+    printf("recieved %d\n", event);
+    std::optional<::Command> command;
+    if (event == 0) {
+      // Timed out.
+      current_state = dest_state;
+      time_when_state_change_done = at_the_end_of_time;
+    } else if (event & HomeAssistantService::kCmdStop) {
+      command = ::Command::My;
+      current_state = dest_state = HomeAssistantService::kStopped;
+      time_when_state_change_done = at_the_end_of_time;
+    } else if (event & HomeAssistantService::kCmdOpen) {
+      // In my deployment, what we want isn't full-open. The "My" command is
+      // programmed to stop the awning at the right spot.
+      command = ::Command::My;
+      current_state = HomeAssistantService::kOpening;
+      dest_state = HomeAssistantService::kOpen;
+      time_when_state_change_done = delayed_by_ms(get_absolute_time(), 5000);
+    } else {
+      assert(event & HomeAssistantService::kCmdClose);
+      command = ::Command::Up;
+      current_state = HomeAssistantService::kClosing;
+      dest_state = HomeAssistantService::kClosed;
+      time_when_state_change_done = delayed_by_ms(get_absolute_time(), 5000);
     }
 
-    printf("Sending command:%hhu\n", static_cast<byte>(message.command));
-    printf("Sending commands disabled during debugging\n");
-    remote.sendCommand(message.command, 1);
-
-    driver.SetMode(RfmSpiDriver::kSleep);
-    last_message = message;
-    last_message_time = receive_time;
-    DrainCommandQueue();
-
-    printf("command sent, waiting for next command\n");
+    if (command) {
+      freertosxx::MutexLock lock(g_controller_mutex);
+      g_somfy_controller->SendCommand(*command);
+    }
+    (*service)->SetState(current_state);
   }
 }
 
 void create_tasks() {
+  g_somfy_controller = SomfyController::Create();
   xTaskCreate(button_task, "button_task", 512, nullptr, 1, nullptr);
-  xTaskCreate(mqtt_task, "mqtt_task", 512, nullptr, 1, nullptr);
-  xTaskCreate(radio_task, "radio_task", 512, nullptr, 2, nullptr);
+  xTaskCreate(
+      homeassistant_task, "homeassistant_task", 512, nullptr, 2, nullptr);
   vTaskDelete(nullptr);
 }
 
 }  // namespace
 }  // namespace jagsawning
 
-extern "C" void main_task(void *) { jagsawning::create_tasks(); }
+extern "C" void main_task(void*) { jagsawning::create_tasks(); }
